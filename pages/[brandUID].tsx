94066f03cf5d238bd0234462d3b8b60f40858bf0
Rob <535rob@gmail.com>
2022-04-24 16:56:30 -1000

    Commit of [brandUID].tsx before editing

---
+++ pages/[brandUID].tsx
@@ -0,0 +1,65 @@
+import { InferGetServerSidePropsType, GetServerSideProps, GetServerSidePropsContext } from 'next';
+
+// React core.
+import React from 'react';
+
+import { doc, DocumentData, getDoc } from "firebase/firestore";
+
+import {db} from '../pages/_app'
+
+export const getServerSideProps:GetServerSideProps = async (context: GetServerSidePropsContext ) => {
+
+        const docRef = doc(db, 'products', 'brandRouteParameters');
+        const docSnap = await getDoc(docRef);
+        let brandDirectoryData:DocumentData = {}
+        let brandStringArray:String[] = []
+
+        if (docSnap.exists()) {
+
+            brandDirectoryData = docSnap.data();
+            Object.keys(brandDirectoryData).forEach((name) => {
+              console.log(name, brandDirectoryData[name]);
+              brandStringArray.push(name)
+            });
+        
+        } else {
+            console.log("No such document!");
+        }
+        
+        return {
+            props: {
+                brandStringArray: brandStringArray
+            }
+        }
+          
+}
+
+function ListOfBrandLinks({brandStringArray}:{brandStringArray:string[]}) {
+    return (
+        <div>
+            <ul>
+                {brandStringArray.map((brand) => (
+                    <li key={brand}>
+                        <a href={`/brands/${brand}`}>{brand}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+
+function BrandDirectoryPage(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+    return(
+        <div>
+            <ListOfBrandLinks brandStringArray={props.brandStringArray}></ListOfBrandLinks>
+        </div>        
+    )
+
+}
+
+
+export default BrandDirectoryPage
+
+           
