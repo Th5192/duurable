b9231baf8d356c1b53ccad64e29d4c446ab6c520
Rob <535rob@gmail.com>
2022-05-04 10:38:05 -1000

    Content Moderation links to Review Editor now

--- pages/content-moderator.tsx
+++ pages/content-moderator.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 // React core.
 import React, { useContext, useEffect, useState } from 'react';
 
-import { collection, doc, getDoc, getDocs, limit, query, where } from "firebase/firestore";
+import { collection, getDocs, limit, query, where } from "firebase/firestore";
 
 import {db} from '../pages/_app'
 
@@ -15,8 +15,21 @@ function ContentModeratorPage(){
 
      let userContext = useContext(UserContext)
      const [userIsAdmin, setUserIsAdmin] = useState(false)
-     const [unmoderatedDocID, setUnmoderatedDocID] = useState('')
-     const [unmoderatedDataPointExists, setUnmoderatedDataPointExists] = useState(false)
+     const [unmoderatedDataPointExists, setUnmoderatedDataPointExists] = useState<boolean>()
+
+     const [dataPointUID, setDataPointUID] = useState('');
+     const [authorUID, setAuthorUID] = useState('');
+     const [brandName, setBrandName] = useState('');
+     const [comments, setComments] = useState('');
+     const [gTIN, setGTIN] = useState('');
+     const [identifierExists, setIdentifierExists] = useState<boolean>();
+     const [itemModelNumber, setItemModelNumber] = useState('');
+     const [timeToReplaceInDays, setTimeToReplaceInDays] = useState<number>();
+     const [title, setTitle] = useState('');
+     const [youTubeURL, setYouTubeURL] = useState('');
+     const [needsReplacement, setNeedsReplacement] = useState<boolean>();
+     const [purchaseDate, setPurchaseDate] = useState<string>('');
+     const [requiredReplacementDate, setRequiredReplacementDate] = useState<string>('');
 
      useEffect(() => {
           console.log('content-moderator useEffect triggered now...')
@@ -40,6 +53,19 @@ function ContentModeratorPage(){
                setUnmoderatedDataPointExists(true)
                querySnapshot.forEach((doc) => {
                     console.log(doc.id, ' => ', doc.data());
+                    setDataPointUID(doc.data().dataPointUID) 
+                    setAuthorUID(doc.data().authorUID)
+                    setBrandName(doc.data().brand)
+                    setComments(doc.data().comments)
+                    setGTIN(doc.data().gTIN)
+                    setIdentifierExists(doc.data().identifierExists)
+                    setItemModelNumber(doc.data().itemModelNumber)
+                    setTimeToReplaceInDays(doc.data().timeToReplaceInDays)
+                    setTitle(doc.data().title)
+                    setYouTubeURL(doc.data().youTubeURL)
+                    setNeedsReplacement(doc.data().needsReplacement)
+                    setPurchaseDate(doc.data().purchaseDate)
+                    setRequiredReplacementDate(doc.data().requiredReplacementDate)
                });
           }
 
@@ -52,6 +78,38 @@ function ContentModeratorPage(){
                     <div>
                          <p>User Is Admin</p>
                          <button onClick={()=>{fetchNotModeratedYetDataPoints()}}>Get next unmoderated data point</button>
+                         <div>
+                              {(unmoderatedDataPointExists === true) &&
+                                   <div>
+                                        <Link
+                                             href={{
+                                             pathname: `/review-editor`,
+                                             query: {
+                                                  dataPointUID: dataPointUID,
+                                                  authorUID: authorUID,
+                                                  brandName: brandName,
+                                                  comments: comments,
+                                                  gTIN: gTIN,
+                                                  identifierExists: identifierExists,
+                                                  itemModelNumber: itemModelNumber,
+                                                  timeToReplaceInDays: timeToReplaceInDays,
+                                                  title: title,
+                                                  youTubeURL: youTubeURL,
+                                                  needsReplacement: needsReplacement,
+                                                  purchaseDate: purchaseDate,
+                                                  requiredReplacementDate: requiredReplacementDate            
+                                             },
+                                             }}>
+                                             <a>Go to unmoderated data point</a>
+                                        </Link>
+                                   </div>
+                              }
+                              {(unmoderatedDataPointExists === false) &&
+                                   <div>
+                                        <p>All data points have been moderated</p>
+                                   </div>
+                              }
+                         </div>
                     </div>
                }
                {(userIsAdmin === false) && 
@@ -65,26 +123,3 @@ function ContentModeratorPage(){
 
 export default ContentModeratorPage
 
-/*
-<Link
-href={{
-    pathname: `/review-editor`,
-    query: {
-        dataPointUID: props.dataPointUID,
-        authorUID: props.authorUID,
-        brandName: props.brandName,
-        comments: props.comments,
-        gTIN: props.gTIN,
-        identifierExists: props.identifierExists,
-        itemModelNumber: props.itemModelNumber,
-        timeToReplaceInDays: props.timeToReplaceInDays,
-        title: props.title,
-        youTubeURL: props.youTubeURL,
-        needsReplacement: props.needsReplacement,
-        purchaseDate: props.purchaseDate,
-        requiredReplacementDate: props.requiredReplacementDate            
-    },
-}}>
-    <a>Edit this data point</a>
-</Link>
-*/
